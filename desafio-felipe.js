d70d3e44aa8c548202383a91b75afe7999a182ec
joyce guntzel cursi <joycecursi@gmail.com>
2024-10-25 21:20:28 -0300

    Update desafio-felipe.js

---
+++ desafio-felipe.js
@@ -0,0 +1,48 @@
+// Variáveis para armazenar o nome do herói e a quantidade de experiência (XP)
+let nome = "artemis"; // Substitua "HeroName" pelo nome desejado
+let experiencia = 8000; // Substitua 4500 pelo valor de XP desejado
+
+// Variável para armazenar o nível do herói
+let nivel;
+
+// Estrutura de decisão para classificar o nível do herói com base na experiência
+if (experiencia < 1000) {
+    nivel = 
+  
+"Ferro";
+} 
+
+else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = 
+    nivel = 
+"Bronze";
+} 
+else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = 
+    nivel 
+"Prata";
+} 
+ 
+else if (experiencia >= 5001 && experiencia <= 7000) {
+    nivel = 
+    nivel = 
+"Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = 
+    ni
+"Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = 
+ 
+"Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = 
+    nivel = 
+"Imortal";
+}  
+else {
+    nivel = "Radiante";
+}
+
+// Exibindo a mensagem final
+console.log(`O Herói de nome ${artemis} está no nível de ${ouro}`);
